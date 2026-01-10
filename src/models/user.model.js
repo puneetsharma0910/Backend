27f61785caf7745a8147e973e1b1f257b5823247
Puneet Sharma <puneetsharma0910@gmail.com>
2026-01-10 23:26:09 +0530

    - Enhanced user.model.js with pre-middlwware , refresh/access jwtToken

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,19 +56,32 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasseordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sign({
-    _id: this._id,
-    fullName : this.fullName,
-    email : this.email,
+  return jwt.sign(
+    {
+      _id: this._id,
+      fullName: this.fullName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
-  }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  });
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
